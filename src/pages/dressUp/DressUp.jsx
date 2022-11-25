80cf0f30d0633d184f8dce7a540bae1a68d5f8e5
harin0707 <yuharin0707@gmail.com>
2022-11-26 03:44:40 +0900

    refactor: 조건문 순서 바꿈

--- src/pages/dressUp/DressUp.jsx
+++ src/pages/dressUp/DressUp.jsx
@@ -43,9 +43,9 @@ const DressUp = () => {
     return (
         <>
         <ContainerE>
-            <Header title={(message===true)?'눈사람 꾸미기':'메세지 남기기'}/>
+            <Header title={message?'메세지 남기기':'눈사람 꾸미기'}/>
             <DressZone>
-                <SnowMan imgSize={(message===true)?'15':'19.375'} />
+                <SnowMan imgSize={message?'15':'19.375'} />
 
                 {message === false ?
                 <>

0d062fcda3482ccc008c1cc33bbd79fd256f619a
nnnayy <kny5439@cau.ac.kr>
2022-11-28 15:01:39 +0900

    feat : 시작 모달창 완료, 공유 및 확인 모달창 수정 예정

--- src/pages/dongsan/StaticGrid.jsx
+++ src/pages/dongsan/StaticGrid.jsx
@@ -2,40 +2,46 @@ import React from "react";
 import GridLayout from "react-grid-layout";
 import styled from "styled-components";
 
-import base from "../../asset/img/base.png";
+// import base from "../../asset/img/base.png";
 import { MiddleButton } from "../../styles/globalStyle";
+// import ShareModal from "./ShareModal";
+//import StartModal from "./StartModal";
+// import CheckModal from "./CheckModal";
 
 class StaticGrid extends React.Component {
   render() {
     return (
       <StGridWrapper>
-        <GridLayout className="layout" cols={2} rowHeight={57.25} width={413}>
+        {/* <StartModal /> */}
+        {/* <ShareModal /> */}
+        {/* <CheckModal /> */}
+        <GridLayout className="layout" cols={2} rowHeight={60.85} width={413}>
           <div
             key="a"
             data-grid={{ x: 0, y: 0, w: 2, h: 2, static: true }}
           ></div>
           <div key="b" data-grid={{ x: 0, y: 2, w: 1, h: 3, static: true }}>
-            <StImg src={base} alt="민둥 눈사람입니다." />
+            {/* <StImg src={base} alt="민둥 눈사람입니다." /> */}
           </div>
           <div
             key="c"
             data-grid={{ x: 2, y: 2, w: 1, h: 1, static: true }}
           ></div>
           <div key="d" data-grid={{ x: 3, y: 3, w: 1, h: 3, static: true }}>
-            <StImg src={base} alt="눈사람입니다." />
+            {/* <StImg src={base} alt="눈사람입니다." /> */}
           </div>
           <div key="e" data-grid={{ x: 0, y: 5, w: 1, h: 3, static: true }}>
-            <StImg src={base} alt="눈사람입니다." />
+            {/* <StImg src={base} alt="눈사람입니다." /> */}
           </div>
           <div
             key="f"
             data-grid={{ x: 6, y: 6, w: 1, h: 1, static: true }}
           ></div>
           <div key="g" data-grid={{ x: 7, y: 7, w: 1, h: 3, static: true }}>
-            <StImg src={base} alt="눈사람입니다." />
+            {/* <StImg src={base} alt="눈사람입니다." /> */}
           </div>
           <div key="h" data-grid={{ x: 0, y: 8, w: 1, h: 3, static: true }}>
-            <StImg src={base} alt="눈사람입니다." />
+            {/* <StImg src={base} alt="눈사람입니다." /> */}
           </div>
           <div
             key="i"
@@ -47,7 +53,7 @@ class StaticGrid extends React.Component {
             style={{
               display: "flex",
               justifyContent: "center",
-              marginTop: "30.25px",
+              marginTop: "11.25px",
             }}
           >
             <MiddleButton>내 동산 공유하기</MiddleButton>
@@ -63,9 +69,8 @@ export default StaticGrid;
 const StGridWrapper = styled.section`
   background-image: url("image/background1.png");
   background-size: 430px;
-  padding-left: 8px;
 `;
 
-const StImg = styled.img`
-  width: 192px;
-`;
+// const StImg = styled.img`
+//   width: 192px;
+// `;

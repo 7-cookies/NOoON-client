fd4e7c8317330490409ee62e4480c304df3c60d5
harin0707 <yuharin0707@gmail.com>
2022-12-09 00:15:59 +0900

    chore: letter, creater POST로 전달

--- src/components/DressUp/WRMessage.jsx
+++ src/components/DressUp/WRMessage.jsx
@@ -1,25 +1,66 @@
 import React, {useState, useRef} from 'react'
 import styled from 'styled-components'
-import { Input, ShortButton} from "../../styles/globalStyle";
+import { Input, ShortButton, Container} from "../../styles/globalStyle";
 import {useRecoilState} from 'recoil'
-import {WRMessageRecoil} from '../../utils/dressRecoil';
-
+import axios from 'axios'
+import {baseEyes, baseArms, baseHead, baseNose, baseItem, baseMouth} from '../../utils/dressRecoil'
+import { useNavigate } from 'react-router-dom';
 
 
 const WRMessage = () => {
+    const navigate=useNavigate();
     const createrRef=useRef(null)
     const messageRef=useRef(null)
     const [messageContents, setMessageContents] = useState({letter:'', creater:''});
+    const [saveFirst, setSaveFirst] = useState(false);
+    const [saveComp, setSaveComp] = useState(false);
+    const isClickedSaveBt = () =>{
+        setSaveFirst(!saveFirst);
+    }
+    const isCompSaved = () =>{
+        setSaveComp(!saveComp)
+        setSaveFirst(false);
+
+    }
+
+    const [invitationCode, setInvitationCode] = useState('')
+    const [eye, setEyes] = useRecoilState(baseEyes);
+    const [nose, setNose] = useRecoilState(baseNose);
+    const [arm, setArms] = useRecoilState(baseArms);
+    const [item, setItem] = useRecoilState(baseItem);
+    const [mouth, setMouth] = useRecoilState(baseMouth);
+    const [head, setHead] = useRecoilState(baseHead);
 
     const saveMessage=()=>{
         setMessageContents((prev)=>({...prev, letter:messageRef.current.value, creater: createrRef.current.value}));
-
     }
+
+
+    const handleSubmit =()=>{
+        axios.post(
+            `${process.env.REACT_APP_BE_SERVER_DOMAIN}api/v1/place/${invitationCode}/snowman`,
+            {
+                "head": `${head[0].Head}`,
+                "accessary": `${item[0].Item}`,
+                "eye": `${eye[0].Eye}`,
+                "nose": `${nose[0].Nose}`,
+                "mouse": `${mouth[0].Mouth}`,
+                "arm": `${arm[0].Eye}`,
+                "letter" : `${messageContents.letter}`,
+                "creator": `${messageContents.creater}`
+            },
+        )
+        .then((response) => {
+            navigate("/grid");
+        });
+    }
+
     
 
 
     console.log(messageContents);
     return (
+        <>
         <TextZone>
             <WriterCon>
                 <div>
@@ -40,10 +81,39 @@ const WRMessage = () => {
                 </div>
             </ContentsCon>
 
-            <ShortButton onClick={saveMessage}> 저장 </ShortButton>
+            <ShortButton onClick={()=>{saveMessage(); isClickedSaveBt()}}> 저장 </ShortButton>
 
 
         </TextZone>
+
+        <ContainerEdit modal={saveComp ? true : (saveFirst ? true: false)}>
+        {saveFirst?
+            <ModalCon> 
+            <TextCon>
+                <div> 저장한 눈사람과 메세지는 수정할 수 없습니다. </div>
+                <div className='save'> 저장하시겠습니까? </div>
+            </TextCon>
+            <ButtonCon>
+                <ShortButtonE onClick={handleSubmit}>확인</ShortButtonE>
+                <ShortButtonE onClick={isClickedSaveBt}>취소</ShortButtonE>
+            </ButtonCon>
+        </ModalCon> : <></>    
+        }
+
+        {saveComp?
+            <ModalCon className='compSaved'> 
+            <TextCon>
+                <div> 저장되었습니다. </div>
+            </TextCon>
+            <ButtonCon>
+                <ShortButtonE onClick={isCompSaved}>확인</ShortButtonE>
+            </ButtonCon>
+        </ModalCon> : <></>    
+        }
+
+
+        </ContainerEdit>
+        </>
 
     )
 }
@@ -110,7 +180,55 @@ margin-bottom: 2.6875rem;
 ::placeholder{
     color: #B4B4B4;
 }
+`
 
+const ContainerEdit = styled(Container)`
+background: ${(props)=>props.modal===false ? 'transparent' :'rgba(85, 85, 85, 0.2)'};
+z-index: ${(props)=>props.modal===false ? -100 :100};
+position: absolute;
+
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+
+const ModalCon = styled.section`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 23.875rem;
+height: 15.1875rem;
+
+background-color: #FFFFFF;
+position: fixed;
+margin-bottom: 10rem;
+border-radius: 1.0625rem;
+
+box-shadow: 0.3vw 0.3vw 0.6vw rgba(0, 0, 0, 0.3);
+`
+
+const TextCon = styled.section`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+& > div{
+    ${({ theme }) => theme.fonts.kotrahopeText}
+    font-size: 1.25rem;
+    margin-top: 0.6rem;
+}
+`
+
+const ButtonCon = styled.section`
+    display: flex;
+    margin-top: 2rem;
+`
+
+const ShortButtonE = styled(ShortButton)`
+margin: 0 1rem;;
 `
 
 
--- src/pages/dressUp/DressUp.jsx
+++ src/pages/dressUp/DressUp.jsx
@@ -1,9 +1,6 @@
 import React, {useState} from 'react';
 import styled from 'styled-components'
-import { useNavigate } from 'react-router-dom';
 import {useRecoilState, useRecoilValue} from 'recoil'
-import axios from 'axios'
-import {baseEyes, baseArms, baseHead, baseNose, baseItem, baseMouth} from '../../utils/dressRecoil'
 
 import {Container, ShortButton} from '../../styles/globalStyle';
 import {DressZone} from '../../styles/dressUp/DressUp.jsx'
@@ -28,54 +25,6 @@ const DressUp = () => {
     }
 
 
-    const [saveFirst, setSaveFirst] = useState(false);
-    const [saveComp, setSaveComp] = useState(false);
-    const isClickedSaveBt = () =>{
-        setSaveFirst(!saveFirst);
-        // console.log(saveFirst);
-    }
-    
-
-    const isCompSaved = () =>{
-        setSaveComp(!saveComp)
-        setSaveFirst(false);
-
-    }
-
-    const navigate=useNavigate();
-    const [invitationCode, setInvitationCode] = useState('')
-    const [eye, setEyes] = useRecoilState(baseEyes);
-    const [nose, setNose] = useRecoilState(baseNose);
-    const [arm, setArms] = useRecoilState(baseArms);
-    const [item, setItem] = useRecoilState(baseItem);
-    const [mouth, setMouth] = useRecoilState(baseMouth);
-    const [head, setHead] = useRecoilState(baseHead);
-
-    const handleSubmit =()=>{
-        
-    
-        // console.log(head[0].Head)
-        
-    
-        axios.post(
-            `${process.env.REACT_APP_BE_SERVER_DOMAIN}api/v1/place/${invitationCode}/snowman`,
-            {
-                "head": `${head[0].Head}`,
-                "accessary": `${item[0].Item}`,
-                "eye": `${eye[0].Eye}`,
-                "nose": `${nose[0].Nose}`,
-                "mouse": `${mouth[0].Mouth}`,
-                "arm": `${arm[0].Eye}`,
-                "letter" : "안녕하세요. 크리스마스 너무 좋아. 두리주아 어어어 두리주아",
-                "creator": "유난"
-            },
-        )
-        .then((response) => {
-            navigate("/grid");
-        });
-    }
-
-    
     return (
         <>
         <ContainerE>
@@ -103,40 +52,13 @@ const DressUp = () => {
 
                 </> :
                 <>
-                <WRMessage modalFirst={saveFirst} modalSecond={saveComp}/>
-                <ShortButton onClick={isClickedSaveBt}> 저장 </ShortButton>
+                <WRMessage />
                 </>
                 }
 
             </DressZone>
         </ContainerE >
-        <ContainerEdit modal={saveComp ? true : (saveFirst ? true: false)}>
-            {saveFirst?
-                <ModalCon> 
-                <TextCon>
-                    <div> 저장한 눈사람과 메세지는 수정할 수 없습니다. </div>
-                    <div className='save'> 저장하시겠습니까? </div>
-                </TextCon>
-                <ButtonCon>
-                    <ShortButtonE onClick={handleSubmit}>확인</ShortButtonE>
-                    <ShortButtonE onClick={isClickedSaveBt}>취소</ShortButtonE>
-                </ButtonCon>
-            </ModalCon> : <></>    
-        }
-
-            {saveComp?
-                <ModalCon className='compSaved'> 
-                <TextCon>
-                    <div> 저장되었습니다. </div>
-                </TextCon>
-                <ButtonCon>
-                    <ShortButtonE onClick={isCompSaved}>확인</ShortButtonE>
-                </ButtonCon>
-            </ModalCon> : <></>    
-            }
-
-            
-        </ContainerEdit>
+        
         
         </>
             
@@ -155,55 +77,9 @@ margin-top: 43rem;
 z-index: 1;
 `
 
-const ContainerEdit = styled(Container)`
-background: ${(props)=>props.modal===false ? 'transparent' :'rgba(85, 85, 85, 0.2)'};
-z-index: ${(props)=>props.modal===false ? -100 :100};
-position: absolute;
 
-display: flex;
-justify-content: center;
-align-items: center;
-
-`
 
 const ContainerE = styled.section`
 position: absolute;
 `
 
-const ModalCon = styled.section`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width: 23.875rem;
-height: 15.1875rem;
-
-background-color: #FFFFFF;
-position: fixed;
-margin-bottom: 10rem;
-border-radius: 1.0625rem;
-
-box-shadow: 0.3vw 0.3vw 0.6vw rgba(0, 0, 0, 0.3);
-`
-
-const TextCon = styled.section`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-
-& > div{
-    ${({ theme }) => theme.fonts.kotrahopeText}
-    font-size: 1.25rem;
-    margin-top: 0.6rem;
-}
-`
-
-const ButtonCon = styled.section`
-    display: flex;
-    margin-top: 2rem;
-`
-
-const ShortButtonE = styled(ShortButton)`
-margin: 0 1rem;;
-`

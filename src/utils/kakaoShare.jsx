7ddcfe2b6ef94666086a2ac9e2ff32b85b09d83f
Yu Harin <yuharin0707@naver.com>
2022-12-10 05:14:18 +0900

    refactor: 카카오 공유하기 이미지 조정

--- src/utils/kakaoShare.jsx
+++ src/utils/kakaoShare.jsx
@@ -25,7 +25,7 @@ export const KakaoShare = ({url}) => {
         content: {
           title: "크리스마스에 완성되는 나만의 눈동산",
           description: `${Name}님의 눈동산에 방문해 눈사람을 꾸미고 메시지를 남겨주세요!⛄️`,
-          imageUrl: `${process.env.REACT_APP_S3_URL}%E1%84%8C%E1%85%A6%E1%84%86%E1%85%A9%E1%86%A8_%E1%84%8B%E1%85%A5%E1%86%B9%E1%84%82%E1%85%B3%E1%86%AB_%E1%84%8B%E1%85%A1%E1%84%90%E1%85%B3%E1%84%8B%E1%85%AF%E1%84%8F%E1%85%B3+4+15.png`,
+          imageUrl: `${process.env.REACT_APP_S3_URL}kakaoTitle.png`,
           link: {
             webUrl: window.location.href+`/${url}`,
             mobileWebUrl: window.location.href+`/${url}`,

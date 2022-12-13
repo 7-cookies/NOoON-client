1e7c240222e2e425f9f6bf61d83bc47d0c3a7c48
Yu Harin <yuharin0707@naver.com>
2022-12-13 18:00:25 +0900

    refactor: 내동산 2,4 동산 이름 색상 조정

--- src/pages/dongsan/GridFix.jsx
+++ src/pages/dongsan/GridFix.jsx
@@ -78,6 +78,7 @@ const GridFix = () => {
       setBackground(response.data.data.background);
       setTitle(response.data.data.name);  
       setIC(response.data.data.invitationCode);
+      console.log(background)
     }catch(error){
       if(error.response && error.response.status === 400){
         const response = await axios.get(
@@ -145,7 +146,7 @@ const GridFix = () => {
         </StModalWrapper>
       )}
 
-      <H1 background={background}>
+      <H1>
         {title}
       </H1>
 
@@ -236,6 +237,7 @@ const H1 = styled.h1`
 
 
   ${({ theme }) => theme.fonts.kotrahopeTitle}
+  /* color: ${(props) => ((props.background === 2 || props.background === 4)  ? "#877C73" : "white")}; */
   color: ${(props) => ((props.background === 2 || props.background === 4)  ? "#877C73" : "white")};
 `;
 
